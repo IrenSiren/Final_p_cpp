840e172e9e2d4ec252ce8d69a8637aaa5b1d4ed1
IrenSiren <71807032+IrenSiren@users.noreply.github.com>
2021-12-10 15:10:18 +0300

    Create bonus.cpp

---
+++ proj/bonus.cpp
@@ -0,0 +1,26 @@
+#include "bonus.h"
+#include <QTimer>
+#include <QGraphicsScene>
+#include <QList>
+#include <QDebug>
+#include <QRandomGenerator>
+#include <QTransform>
+
+
+Bonus::Bonus(QGraphicsItem *parent): QObject(), QGraphicsPixmapItem(parent)
+{
+    //set random x, y position
+    QRandomGenerator *rg = QRandomGenerator::global();
+    int random_number_x = rg->bounded(50,650);
+    int random_number_y = rg->bounded(50,450);
+    setPos( random_number_x, random_number_y );
+
+    // drew the rect
+    QTransform trans;
+    QPixmap pic;
+    pic = QPixmap(":/images/bonbon_2.png");
+    pic = pic.scaled(60,60, Qt::IgnoreAspectRatio);
+    setPixmap(pic);
+    int bonus = 10;
+
+}
